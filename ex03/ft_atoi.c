127bcca5e9a455e6d136fe1efec33ed3d47b6727
taehykim <tehyoyee@gmail.com>
2021-09-20 21:21:36 +0900

    21/09/20  //  C04  //  v1.01

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taehykim <taehykim@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 20:45:21 by taehykim          #+#    #+#             */
+/*   Updated: 2021/09/20 21:20:08 by taehykim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == ' ' || (9 <= *str && *str <= 13))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while ('0' <= *str && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (result * sign);
+}
+
+int	main(void)
+{
+	char	str[15] = "  -+-15a5";
+	printf("%d", ft_atoi(str));
+}
